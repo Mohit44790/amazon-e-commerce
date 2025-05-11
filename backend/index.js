e0a08a46d5955a307db2f1bd8333689fc331fbf1
mohit44790 <mohit44790@gmail.com>
2025-05-11 12:53:07 +0530

    amazon update vercel

--- backend/index.js
+++ backend/index.js
@@ -39,5 +39,6 @@ app.use("/api/sales", salesRoutes);
 app.use("/api/giftcards", giftcardsRoutes);
 app.use("/api/grocery", groceryRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
