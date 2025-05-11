b30addbf654b696b2fc2aa7d51e9a595278b38f5
mohit44790 <mohit44790@gmail.com>
2025-05-11 14:22:25 +0530

    amazon update vercel

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import serverless from 'serverless-http';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -41,4 +42,5 @@ app.use("/api/grocery", groceryRoutes);
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-export default app;
+
+export default serverless(app); 
