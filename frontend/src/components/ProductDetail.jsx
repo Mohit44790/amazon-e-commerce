f3eeba6318cf0eb95cae17da5b2a9baf2f98175b
mohit44790 <mohit44790@gmail.com>
2025-05-11 12:08:07 +0530

    amazon update

--- frontend/src/components/ProductDetail.jsx
+++ frontend/src/components/ProductDetail.jsx
@@ -6,7 +6,9 @@ import { fetchProductById } from '../redux/slice/productSlice';
 import { addToCart } from '../redux/slice/cartSlice';
 import { toast } from 'react-toastify';
 import ProductReviews from './ProductReviews';
-import ReactImageMagnify from 'react-image-magnify';
+// import ReactImageMagnify from 'react-image-magnify';
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 const ProductDetail = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -93,8 +95,16 @@ const ProductDetail = () => {
               className="w-96 h-full object-cover border rounded"
             />
           </div> */}
+          <Zoom>
+    <img
+      src={selectedImage || product?.images?.[0]}
+      onError={(e) => (e.target.src = '/placeholder.jpg')}
+      alt="Selected"
+      className="w-full h-auto object-cover border rounded"
+    />
+  </Zoom>
           {/* Main Image with Zoom */}
-<div className="flex flex-col h-full items-center gap-2 w-[400px]">
+{/* <div className="flex flex-col h-full items-center gap-2 w-[400px]">
   <ReactImageMagnify
     {...{
       smallImage: {
@@ -116,7 +126,7 @@ const ProductDetail = () => {
       shouldUsePositiveSpaceLens: true,
     }}
   />
-</div>
+</div> */}
 
 
           {/* Product Info */}
