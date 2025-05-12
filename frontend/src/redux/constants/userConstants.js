75b2f9d29eb5ef07834c9e57c2bed1ad2f444403
mohit44790 <mohit44790@gmail.com>
2025-05-12 11:41:11 +0530

    amazon update 5

--- frontend/src/redux/constants/userConstants.js
+++ frontend/src/redux/constants/userConstants.js
@@ -1,3 +1,3 @@
 
-//export const USER_API_END_POINT="http://localhost:8000/api";
-export const USER_API_END_POINT="https://amazon-e-commerce-ckel.vercel.app/api";
+export const USER_API_END_POINT="http://localhost:8000/api";
+// export const USER_API_END_POINT="https://amazon-e-commerce-ckel.vercel.app/api";
