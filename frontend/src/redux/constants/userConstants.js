509d871d9a439185d299a709c200a85b8c2a78ca
Mohit44790 <120947455+Mohit44790@users.noreply.github.com>
2025-06-06 15:45:26 +0530

    Update userConstants.js

--- frontend/src/redux/constants/userConstants.js
+++ frontend/src/redux/constants/userConstants.js
@@ -1,4 +1,4 @@
 
-export const USER_API_END_POINT="http://localhost:8000/api";
-// export const USER_API_END_POINT="https://e-comm-backend-mu.vercel.app/api";
+// export const USER_API_END_POINT="http://localhost:8000/api";
+export const USER_API_END_POINT="https://e-comm-backend-mu.vercel.app/api";
 
